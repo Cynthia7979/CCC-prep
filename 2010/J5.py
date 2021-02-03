1f481a124a9194124db18d14c2a0d3200fc0dbcd
BuildTools <CynthiaWang7979@hotmail.com>
2021-02-03 18:28:29 +0800

    2010/J5 last greedy

--- 2010/J5.py
+++ 2010/J5.py
@@ -1,42 +1,62 @@
 import math
 
-x0, y0 = [int(i) for i in input().split()]
-x1, y1 = [int(i) for i in input().split()]
+
+def main():
+    x0, y0 = [int(i) for i in input().split()]
+    x1, y1 = [int(i) for i in input().split()]
+
+    if (x0, y0) == (x1, y1):
+        print(0)
+        return
+
+    # Try Greedy
+    x_now, y_now = x0, y0
+    steps = 0
+
+    while (x_now, y_now) != (x1, y1):
+        current_min = None
+        current_min_distnce = None
+        found = False
+        for delta_x in (2, -2):
+            if found: break
+            for delta_y in (1, -1):
+                x, y = x_now+delta_x, y_now+delta_y
+                if 1 <= x <= 8 and 1 <= y <= 8:
+                    distance = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
+                    if current_min_distnce:
+                        if distance < current_min_distnce:
+                            current_min_distnce = distance
+                            current_min = (x, y)
+                    else:
+                        current_min_distnce = distance
+                        current_min = (x, y)
+                    if distance == 0:
+                        found = True
+                        break
+
+        for delta_x in (1, -1):
+            if found: break
+            for delta_y in (2, -2):
+                x, y = x_now+delta_x, y_now+delta_y
+                if 1 <= x <= 8 and 1 <= y <= 8:
+                    distance = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
+                    if current_min_distnce:
+                        if distance < current_min_distnce:
+                            current_min_distnce = distance
+                            current_min = (x, y)
+                    else:
+                        current_min_distnce = distance
+                        current_min = (x, y)
+                    if distance == 0:
+                        found = True
+                        break
+
+        x_now, y_now = current_min
+        # print(x_now, y_now)
+        steps += 1
+
+    print(steps)
 
 
-def next_positions(x, y):
-    pos = []
-    for delta_x in (2, -2):
-        for delta_y in (1, -1):
-            if 1 <= x+delta_x <= 8 and 1 <= y+delta_y <= 8:
-                pos.append((x+delta_x, y+delta_y))
-    for delta_x in (1, -1):
-        for delta_y in (2, -2):
-            if 1 <= x+delta_x <= 8 and 1 <= y+delta_y <= 8:
-                pos.append((x+delta_x, y+delta_y))
-    return pos
-
-
-# Try Greedy
-x_now, y_now = x0, y0
-steps = 0
-
-while (x_now, y_now) != (x1, y1):
-    current_min = None
-    current_min_distnce = None
-    for x, y in next_positions(x_now, y_now):
-        distance = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
-        if current_min_distnce:
-            if distance < current_min_distnce:
-                current_min_distnce = distance
-                current_min = (x, y)
-        else:
-            current_min_distnce = distance
-            current_min = (x, y)
-        if distance == 0:
-            break
-    x_now, y_now = current_min
-    # print(x_now, y_now)
-    steps += 1
-
-print(steps)
+if __name__ == '__main__':
+    main()
